fc118748f298723b231afd16bdd9ea594e23ef45
Tulasibionic <tulasigadini@gmail.com>
2025-05-04 18:00:32 +0530

    ax

--- src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://budget-track-19hu.onrender.com/',
-});
-
-export default instance;
