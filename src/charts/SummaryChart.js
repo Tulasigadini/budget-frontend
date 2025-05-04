0155779700f2dc56512810f7e8e0289d5cf5a0b7
Tulasibionic <tulasigadini@gmail.com>
2025-05-04 18:23:08 +0530

    name graaph

--- src/charts/SummaryChart.js
+++ src/charts/SummaryChart.js
@@ -1,59 +1,69 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-function SummaryChart({ summary }) {
-  const ref = useRef();
+function SummaryChart({ income, expenses }) {
+  const svgRef = useRef();
 
   useEffect(() => {
-    const svg = d3.select(ref.current);
-    svg.selectAll('*').remove();
-
-    const width = 400;
-    const height = 300;
-    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
-
     const data = [
-      { name: 'Income', value: summary.total_income || 0 },
-      { name: 'Expenses', value: summary.total_expenses || 0 },
-      { name: 'Balance', value: summary.balance || 0 },
+      { category: 'Income', value: income },
+      { category: 'Expenses', value: expenses },
     ];
+
+    const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+    const width = 300 - margin.left - margin.right;
+    const height = 200 - margin.top - margin.bottom;
+
+    // Clear previous chart
+    d3.select(svgRef.current).selectAll('*').remove();
+
+    const svg = d3
+      .select(svgRef.current)
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
     const x = d3
       .scaleBand()
-      .domain(data.map((d) => d.name))
-      .range([margin.left, width - margin.right])
-      .padding(0.1);
+      .domain(data.map(d => d.category))
+      .range([0, width])
+      .padding(0.2);
 
     const y = d3
       .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.value)]).nice()
-      .range([height - margin.bottom, margin.top]);
+      .domain([0, d3.max(data, d => d.value) * 1.2])
+      .range([height, 0]);
 
+    // Add bars
     svg
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
-      .attr('transform', `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x));
-
-    svg
-      .append('g')
-      .attr('transform', `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y));
-
-    svg
-      .append('g')
-      .selectAll('rect')
+      .selectAll('.bar')
       .data(data)
-      .join('rect')
-      .attr('x', (d) => x(d.name))
-      .attr('y', (d) => y(d.value))
-      .attr('height', (d) => y(0) - y(d.value))
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => x(d.category))
+      .attr('y', d => y(d.value))
       .attr('width', x.bandwidth())
-      .attr('fill', 'steelblue');
-  }, [summary]);
+      .attr('height', d => height - y(d.value))
+      .attr('fill', '#4682b4');
 
-  return <svg ref={ref}></svg>;
+    // Add Y-axis
+    svg
+      .append('g')
+      .call(d3.axisLeft(y).ticks(5).tickFormat(d3.format('d')));
+
+    // Add X-axis with category labels
+    svg
+      .append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll('text')
+      .style('font-size', '12px')
+      .style('fill', '#333');
+  }, [income, expenses]);
+
+  return <svg ref={svgRef}></svg>;
 }
 
 export default SummaryChart;
